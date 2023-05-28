91c75c3e3493fba5fdc863687d1e07d9a06f07b6
estefany00170 <133134954+estefany00170@users.noreply.github.com>
2023-05-28 10:48:26 -0400

    Add files via upload

---
+++ Evento Onclick/index.js
@@ -0,0 +1,12 @@
+
+function changelogin(){
+    document.getElementById("buttonlogin").innerHTML="Logout";
+}
+
+function removeadd(){
+    document.getElementById("buttonadd").remove();
+}
+
+function ninjalike(){
+    alert("ninja was like");
+}
