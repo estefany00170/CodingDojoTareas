ba8be8abeb4c1bdd9ae45bedc75c670c08e045a5
estefany00170 <133134954+estefany00170@users.noreply.github.com>
2023-06-10 12:24:24 -0400

    Add files via upload

---
+++ video-preview/script.js
@@ -0,0 +1,13 @@
+
+
+function reproducevideo(){
+        var video = document.getElementById("myVideo");
+        video.play();
+        video.muted = true;
+}
+
+function pausaVideo(){
+        var video = document.getElementById("myVideo");
+        video.pause();
+
+}
